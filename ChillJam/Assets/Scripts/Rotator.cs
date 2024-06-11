20e1f4e912c5ae15bef51e724575ec49b99984d4
Zeko Madson <zmadson3@gatech.edu>
2024-06-10 23:20:58 -0400

    Add Coffee Beans

---
+++ ChillJam/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    
+    void Update()
+    {
+        float xRotate = 175f;
+        float yRotate = 0f;
+        float zRotate = 0f;
+        transform.Rotate(new Vector3(xRotate, yRotate,zRotate) * Time.deltaTime);
+    }
+}
